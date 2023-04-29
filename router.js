5cc86078a835aa99a7882e63a56d7eb304404ec8
Miguel <miguellealw@gmail.com>
2023-04-29 17:53:03 -0500

    added comments

--- router.js
+++ router.js
@@ -28,35 +28,34 @@ class RouterNode {
         const neighbors = [];
         // Show routing table
         console.log(`Neighbors of ${ip_to_letter_map[this.ip]}:`);
+
+        // Loop through the router table to find adjacent neighbors
         for (const neighbor in this.routerTable) {
             const cost = this.routerTable[neighbor];
-            // only show adjacent neighbors only
+
+            // Only show adjacent neighbors that are reachable and not self
             if (cost !== "inf" && cost != 0) {
                 neighbors.push(neighbor);
+                // Update forwarding table with neighbor's address
                 this.forwardingTable[neighbor] = neighbor; 
                 console.log(`  ${ip_to_letter_map[neighbor]}: ${cost}`);
             }
         }
 
-
-
         console.log(this.routerTable);
 
+        // Send updates to all neighbors to inform them of this node's address
         for(const messageOf of neighbors) {
             for(const messageTo of neighbors) {
+                // Send an update to the neighbor
                 this.sendUpdate(this.ip, messageOf, this.routerTable[messageTo], messageTo);
             }
         }
-
-
 
         // Set up message listener
         this.socket.on('message', (msg, rinfo) => {
             this.handleMessage(msg, rinfo);
         });
-
-        
-
     }
 
 
@@ -69,14 +68,17 @@ class RouterNode {
         const costToSender = this.routerTable[sender];
         
         const node = message.neighbor;
+
+        // If the forwarding table does not contain the node, add it
         if (! Object.keys(this.forwardingTable).includes(node)) {
             this.forwardingTable[node] = cost;
-        }
-        else {
+        } else {
+            // If the forwarding table already contains the node, update the cost if it is lower
             if(this.forwardingTable[cost] > costToSender + cost) {
                 this.forwardingTable[cost] = costToSender + cost;
                 console.log("Updated cost.")
 
+                // Send an update to all neighbors
                 const messageOf = node;
                 for(const messageTo of neighbors) {
                     this.sendUpdate(this.ip, messageOf, this.routerTable[messageTo], messageTo);
